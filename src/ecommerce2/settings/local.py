fb196e6d1faa928b837a8a1aade6125032964b2e
anthonymiyoro <amiyoro@gmail.com>
2016-10-22 20:00:07 +0300

    Created a products app

--- src/ecommerce2/settings/local.py
+++ src/ecommerce2/settings/local.py
@@ -97,10 +97,15 @@ WSGI_APPLICATION = 'ecommerce2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tony',
+        'USER': 'tony',
+        'PASSWORD': 't@1234',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
 
 
 # Internationalization
