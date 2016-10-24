8215e2f707e625b61d1228c87bfe6d86939bd1a4
anthonymiyoro <amiyoro@gmail.com>
2016-10-24 10:42:50 +0300

    Created basic product list template

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 
@@ -5,8 +6,22 @@ from django.shortcuts import render
 
 from .models import Product
 
+class ProductListView(ListView):
+    model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print context
+        return context
+
+
+class ProductListView(ListView):
+    model = Product
+
+
 class ProductDetailView(DetailView):
     model = Product
+
 
 def product_detail_view_func(request, id):
     product_instance = Product.objects.get(id=id)
