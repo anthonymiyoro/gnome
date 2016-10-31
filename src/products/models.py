a0a5bed0ef9d1c2f037aab8bebaaea649c317fcc
anthonymiyoro <amiyoro@gmail.com>
2016-10-31 21:38:33 +0300

    Created homepage model

--- src/products/models.py
+++ src/products/models.py
@@ -6,79 +6,79 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+
 class ProductQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(active=True)
 
 
 class ProductManager(models.Manager):
-	def get_queryset(self):
-		return ProductQuerySet(self.model, using=self._db)
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
 
-	def all(self, *args, **kwargs):
-		return self.get_queryset().active()
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
 
-	def get_related(self, instance):
-		products_one = self.get_queryset().filter(categories__in=instance.categories.all())
-		products_two = self.get_queryset().filter(default=instance.default)
-		qs = (products_one | products_two).exclude(id=instance.id).distinct()
-		return qs
+    def get_related(self, instance):
+        products_one = self.get_queryset().filter(categories__in=instance.categories.all())
+        products_two = self.get_queryset().filter(default=instance.default)
+        qs = (products_one | products_two).exclude(id=instance.id).distinct()
+        return qs
 
 
 class Product(models.Model):
-	title = models.CharField(max_length=120)
-	description = models.TextField(blank=True, null=True)
-	price = models.DecimalField(decimal_places=2, max_digits=20)
-	active = models.BooleanField(default=True)
-	categories = models.ManyToManyField('Category', blank=True)
-	default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
+    title = models.CharField(max_length=120, default="Title")
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    active = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
 
-	objects = ProductManager()
+    objects = ProductManager()
 
-	class Meta:
-		ordering = ["-title"]
+    class Meta:
+        ordering = ["-title"]
 
-	def __unicode__(self): #def __str__(self):
-		return self.title
+    def __unicode__(self):  # def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse("product_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.pk})
 
-	def get_image_url(self):
-		img = self.productimage_set.first()
-		if img:
-			return img.image.url
-		return img #None
-
+    def get_image_url(self):
+        img = self.productimage_set.first()
+        if img:
+            return img.image.url
+        return img  # None
 
 
 class Variation(models.Model):
-	product = models.ForeignKey(Product)
-	title = models.CharField(max_length=120)
-	price = models.DecimalField(decimal_places=2, max_digits=20)
-	sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
-	active = models.BooleanField(default=True)
-	inventory = models.IntegerField(null=True, blank=True) #refer none == unlimited amount
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120, default="Title")
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    inventory = models.IntegerField(null=True, blank=True)  # refer none == unlimited amount
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def get_price(self):
-		if self.sale_price is not None:
-			return self.sale_price
-		else:
-			return self.price
+    def get_price(self):
+        if self.sale_price is not None:
+            return self.sale_price
+        else:
+            return self.price
 
-	def get_html_price(self):
-		if self.sale_price is not None:
-			html_text = "<span class='sale-price'>%s</span> <span class='og-price'>%s</span>" %(self.sale_price, self.price)
-		else:
-			html_text = "<span class='price'>%s</span>" %(self.price)
-		return mark_safe(html_text)
+    def get_html_price(self):
+        if self.sale_price is not None:
+            html_text = "<span class='sale-price'>%s</span> <span class='og-price'>%s</span>" % (
+            self.sale_price, self.price)
+        else:
+            html_text = "<span class='price'>%s</span>" % (self.price)
+        return mark_safe(html_text)
 
-	def get_absolute_url(self):
-		return self.product.get_absolute_url()
-
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
 
 
 def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
@@ -96,7 +96,7 @@ post_save.connect(product_post_saved_receiver, sender=Product)
 
 
 def image_upload_to(instance, filename):
-    title = instance.product.title
+    title = instance.product.title(default="Title")
     slug = slugify(title)
     basename, file_extension = filename.split(".")
     new_filename = "%s-%s.%s" % (slug, instance.id, file_extension)
@@ -111,9 +111,8 @@ class ProductImage(models.Model):
         return self.product.title
 
 
-
 class Category(models.Model):
-    title = models.Charfield(max_length=120, unique=True)
+    title = models.CharField(max_length=120, unique=True, default="Title")
     slug = models.SlugField(unique=True)
     description = models.TextField(null=True, blank=True)
     active = models.BooleanField(default=True)
@@ -122,36 +121,28 @@ class Category(models.Model):
     def __unicode__(self):
         return self.title
 
+
 # def get_absolute_urls(self):
 #     return reverse("category_detail", kwargs={"slug": self.slug })
 
 def image_upload_to_featured(instance, filename):
-    title = instance.product.title
+    title = instance.product.title(default="Title")
     slug = slugify(title)
     basename, file_extension = filename.split(".")
-    new_filename = "%s-%s.%s" %(slug, instance.id, file_extension)
-    return "products/%s/featured/%s" %(slug, new_filename)
-
+    new_filename = "%s-%s.%s" % (slug, instance.id, file_extension)
+    return "products/%s/featured/%s" % (slug, new_filename)
 
 
 class ProductFeatured(models.Model):
     product = models.ForeignKey(Product)
     image = models.ImageField(upload_to=image_upload_to_featured)
     title = models.CharField(max_length=120, null=True, blank=True)
-	text = models.CharField(max_length=220, null=True, blank=True)
-	text_right = models.BooleanField(default=False)
-	text_css_color = models.CharField(max_length=6, null=True, blank=True)
-	show_price = models.BooleanField(default=False)
-	make_image_background = models.BooleanField(default=False)
-	active = models.BooleanField(default=True)
+    text = models.CharField(max_length=220, null=True, blank=True)
+    text_right = models.BooleanField(default=False)
+    text_css_color = models.CharField(max_length=6, null=True, blank=True)
+    show_price = models.BooleanField(default=False)
+    make_image_background = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.product.title
-
-
-
-
-
-
-
-
