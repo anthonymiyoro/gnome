8276cd6b9ae1da6d83b5104086b02c8ca7f2a152
anthonymiyoro <amiyoro@gmail.com>
2016-10-31 21:29:05 +0300

    Added main page templates

--- src/products/urls_categories.py
+++ src/products/urls_categories.py
@@ -4,12 +4,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
-from .views import ProductDetailView, ProductListView,VariationListView
+from .views import CategoryListView, CategoryDetailView
 
 urlpatterns = [
-
-    url(r'^$', ProductListView.as_view(), name='products'),
-    url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
-    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
-    # url(r'^categories/', include('products.urls_categories')),
+    # Examples:
+    # url(r'^$', 'newsletter.views.home', name='home'),
+    url(r'^$', CategoryListView.as_view(), name='categories'),
+    url(r'^(?P<slug>[\w-]+)/$', CategoryDetailView.as_view(), name='category_detail'),
+    #url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_detail_function'),
 ]
