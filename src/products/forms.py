1af619e03e1272723a7d0b72ff884b2b4b012ae3
anthonymiyoro <amiyoro@gmail.com>
2016-11-10 19:43:52 +0300

    Added admin backend

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from django.forms.models import modelformset_factory
 
-from .models import Variation
+from .models import Variation, Product
 
 
 class VariationInventoryForm(forms.ModelForm):
@@ -17,3 +17,16 @@ class VariationInventoryForm(forms.ModelForm):
 
 
 VariationInventoryFormSet = modelformset_factory(Variation, form=VariationInventoryForm, extra=0)
+
+
+class SellerProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+            "active",
+            "categories",
+            "title",
+        ]
