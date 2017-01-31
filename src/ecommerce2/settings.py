c17cd23ed3bd756b8d37b6b496d6e6bcc953abd8
Anthony Miyoro <amiyoro@gmail.com>
2017-01-31 11:54:05 +0300

    Update settings.py

--- src/ecommerce2/settings.py
+++ src/ecommerce2/settings.py
@@ -133,8 +133,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_r
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_in_pro", "our_static"),
-    os.path.join(BASE_DIR, "static_in_env"),
-    '/var/www/static/',
+    #os.path.join(BASE_DIR, "static_in_env"),
+    #'/var/www/static/',
 )
 
 MEDIA_URL = '/media/'
